bb849db8cd7fe0c4c8939056b63a1ea043488326
Usama Arshad <usama692@hotmail.com>
2017-10-23 10:29:59 +0500

    resolve paid max date set to server date

--- application/views/Rechecking/Footer.php
+++ application/views/Rechecking/Footer.php
@@ -22,7 +22,8 @@
                     dateFormat: 'yy-mm-dd'
                     ,changeMonth: true,changeYear:true
                     , yearRange: '-1:'
-                    ,maxDate: new Date()
+                    //,maxDate: new Date()
+                    ,maxDate: '<?php echo date("Y-m-d"); ?>'
             }).val();
 
             $( "#dob" ).datepicker(
@@ -68,9 +69,9 @@
             alertify.error('You cannot enter more than 20 digits');
             return false;
         }
-      /*  else if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57) && (e.which != 13)) {
-            alertify.error('Please Use Numaric Only');
-            return false;
+        /*  else if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57) && (e.which != 13)) {
+        alertify.error('Please Use Numaric Only');
+        return false;
         }*/
     });
 
